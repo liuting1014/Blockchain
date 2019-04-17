7f8bd4fbe48d6490f42be73305f4a53f162fe767
Ting Liu <ting.liu04@metronom.com>
2019-04-17 16:52:05 +0200

    Add mine_block function and improve verification logic

--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,9 @@
-blockchain = []
+genesis_block = {
+    'previous-hash': '',
+    'index': 0,
+    'transactions': []
+}
+blockchain = [genesis_block]
 open_transactions = []
 owner = 'Ting'
 
@@ -20,7 +25,15 @@ def add_transaction(recipient, sender = owner, amount=1.0):
 
 
 def mine_block():
-    pass
+    last_block = blockchain[-1]
+    hashed_block = hash_block(last_block)
+    print(hashed_block)
+    block = {
+        'previous_hash': hashed_block,
+        'index': len(blockchain),
+        'transactions': open_transactions
+    }
+    blockchain.append(block)
 
 
 def get_transaction_value():
@@ -41,25 +54,24 @@ def print_blockchain_elements():
         print(block)
 
 
+def hash_block(block):
+    return '-'.join([str(block[key]) for key in block])
+
+
 def verify_chain():
-    block_index = 0
-    is_valid = True
-    for block in blockchain:
-        if block_index == 0:
-            block_index += 1
+    for (index, block) in enumerate(blockchain):
+        if index == 0:
             continue
-        elif block[0] == blockchain[block_index - 1]:
-            is_valid = True
-        else:
-            is_valid = False
-            break
-        block_index += 1
-    return is_valid
+        if block['previous_hash'] != hash_block(blockchain[index - 1]):
+            return False
+    return True
+
 
 while True:
     print('Please choose')
     print('1: Add a new transaction value')
-    print('2: Output the blockchain blocks')
+    print('2: Mine a block')
+    print('3: Print blocks')
     print('h: Manipulate the chain')
     print('q: Quit')
     user_choice = get_user_choice()
@@ -67,12 +79,19 @@ while True:
         tx_data = get_transaction_value()
         recipient, amount = tx_data
         add_transaction(recipient, amount=amount)
-        print(open_transactions)
     elif user_choice == '2':
+        mine_block()
+    elif user_choice == '3':
         print_blockchain_elements()
     elif user_choice == 'h':
         if len(blockchain) >= 1:
-            blockchain[0] = [2]
+            blockchain[0] = {
+                'previous_hash': '',
+                'index': 0,
+                'transactions': [
+                    {'sender': 'Whoever', 'recipent': 'me', 'amount': 'whatever'}
+                ]
+            }
     elif user_choice == 'q':
         break
     else:
@@ -82,4 +101,3 @@ while True:
         break
 
 print('Done!')
- 
