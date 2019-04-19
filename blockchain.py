41809af4a0699868712c83207e7155a69b833d25
Ting Liu <liuting901014@gmail.com>
2019-04-19 21:09:13 +0200

    Move utils into separate class

--- blockchain.py
+++ blockchain.py
@@ -1,11 +1,10 @@
-import hashlib
 import pickle
-from collections import OrderedDict
 from time import time
 
 import hash_util
 from block import Block
 from transaction import Transaction
+from utils import Utils
 
 MINING_REWARD = 10
 blockchain = []
@@ -51,20 +50,12 @@ def get_last_blockchain_value():
 
 def add_transaction(recipient, sender=owner, amount=1.0):
 	transaction = Transaction(sender, recipient, amount)
-	if verify_transaction(transaction):
+	verifier = Utils()
+	if verifier.verify_transaction(transaction, get_balance):
 		open_transactions.append(transaction)
 		save_data()
 		return True
 	return False
-
-
-def verify_transaction(transaction):
-	sender_balance = get_balance(transaction.sender)
-	return sender_balance >= transaction.amount > 0
-
-
-def verify_transactions():
-	return all([verify_transaction(tx) for tx in open_transactions])
 
 
 def mine_block():
@@ -98,41 +89,22 @@ def print_blockchain_elements():
 		print(block)
 
 
-def validate_proof(transactions, last_hash, proof):
-	guess = (str([tx.convert_to_ordered_dict() for tx in transactions]) + str(last_hash) + str(proof)).encode()
-	guess_hash = hashlib.sha256(guess).hexdigest()
-	return guess_hash[0:2] == "00"
-
-
 def generate_proof_of_work():
 	last_block = blockchain[-1]
 	last_hash = hash_util.hash_block(last_block)
 	proof = 0
-	while not validate_proof(open_transactions, last_hash, proof):
+	verifier = Utils()
+	while not verifier.validate_proof(open_transactions, last_hash, proof):
 		proof += 1
 	return proof
 
 
-def verify_chain():
-	for (index, block) in enumerate(blockchain):
-		if index == 0:
-			continue
-		if block.previous_hash != hash_util.hash_block(blockchain[index - 1]):
-			return False
-		if not validate_proof(block.transactions[:-1], block.previous_hash, block.proof):
-			print("Proof of work is invalid")
-			return False
-	return True
-
-
 def get_balance(participant):
-	tx_by_sender = [[tx.amount for tx in block.transactions if tx.sender == participant] for block in
-					blockchain]
+	tx_by_sender = [[tx.amount for tx in block.transactions if tx.sender == participant] for block in blockchain]
 	open_tx_by_sender = [tx.amount for tx in open_transactions if tx.sender == participant]
 	tx_by_sender.append(open_tx_by_sender)
 	amount_sent = sum([sum(tx) for tx in tx_by_sender])
-	tx_recipient = [[tx.amount for tx in block.transactions if tx.recipient == participant] for block in
-					blockchain]
+	tx_recipient = [[tx.amount for tx in block.transactions if tx.recipient == participant] for block in blockchain]
 	amount_received = sum([sum(tx) for tx in tx_recipient])
 	return amount_received - amount_sent
 
@@ -159,12 +131,14 @@ while True:
 	elif user_choice == "3":
 		print_blockchain_elements()
 	elif user_choice == "4":
-		verify_transactions()
+		verifier = Utils()
+		verifier.verify_transactions(open_transactions, get_balance)
 	elif user_choice == "q":
 		break
 	else:
 		print("Input was invalid, please pick a value from the list!")
-	if not verify_chain():
+	verifier = Utils()
+	if not verifier.verify_chain(blockchain):
 		print("Invalid blockchain!")
 		break
 	print("Balance of {}: {:*^10.2f}".format(owner, get_balance(owner)))
---
+++ utils.py
@@ -0,0 +1,28 @@
+import hashlib
+
+import hash_util
+
+
+class Utils:
+	def verify_chain(self, blockchain):
+		for (index, block) in enumerate(blockchain):
+			if index == 0:
+				continue
+			if block.previous_hash != hash_util.hash_block(blockchain[index - 1]):
+				return False
+			if not self.validate_proof(block.transactions[:-1], block.previous_hash, block.proof):
+				print("Proof of work is invalid")
+				return False
+		return True
+
+	def verify_transaction(self, transaction, get_balance):
+		sender_balance = get_balance(transaction.sender)
+		return sender_balance >= transaction.amount > 0
+
+	def verify_transactions(self, open_transactions, get_balance):
+		return all([self.verify_transaction(tx, get_balance) for tx in open_transactions])
+
+	def validate_proof(self, transactions, last_hash, proof):
+		guess = (str([tx.convert_to_ordered_dict() for tx in transactions]) + str(last_hash) + str(proof)).encode()
+		guess_hash = hashlib.sha256(guess).hexdigest()
+		return guess_hash[0:2] == "00"
